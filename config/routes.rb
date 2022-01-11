516ec0a01da34492051b32fad004d51400b5b0cd
reona kasuya <reon.kasuya@gmail.com>
2022-01-11 22:07:24 +0900

    validation for review complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'reviews/', to: "reviews#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews
+  end
 end
+
+# resources :reviews, only: [ :new, :create, :index ]
+# this is a limiter when you don't want all the 7 crud actions
