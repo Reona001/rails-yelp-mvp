f5d70ccc8d0d74f02c91b1db1a999cf97ef61ef1
reona kasuya <reon.kasuya@gmail.com>
2022-01-13 19:52:16 +0900

    excercise complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,9 +13,26 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # redirect_to new_restaurant_path(@restaurant)
+      render :new
+    end
   end
+# def create
+#     @review = Review.new(review_params)
+#     @review.restaurant = @restaurant
+#     if @review.save
+#       redirect_to  restaurant_path(@restaurant)
+#     else
+#       render :new
+#     end
+#   end
+
+
+
+
 
   private
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  def new
+    p params
+
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.save
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      @review = Review.new
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
